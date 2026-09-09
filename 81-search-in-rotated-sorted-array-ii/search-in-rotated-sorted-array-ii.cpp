ee463a54064573c0ae3bd97407a20771af3da16c
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2026-09-10 04:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (46.43%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        //  standing at any point half of the vector is sorted so i will check if the 
+        // sorted half contains my answer or not if it does not contain i will move to the next half
+int n = a.size();
+        int lo = 0;
+        int hi = n-1;
+
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(a[mid]==target) return true ;
+            if(a[mid]==a[lo]  && a[mid]== a[hi]){
+                hi--;
+                lo++;
+                continue ;
+            }
+
+            if(a[mid]>=a[lo]){
+            // this half is sorted if check here i can find my target if not move to the next half
+            if(target >=a[lo] && target <= a[mid]){
+                hi = mid-1 ;
+            }
+            else{
+                lo = mid+1 ;
+            }
+            }
+            else{
+                // a[hi] >a[mid]
+                if(target >=a[mid] && target <= a[hi]){
+                    lo = mid +1;
+                }
+                else{
+                    hi = mid-1 ;
+                }
+            }
+        }
+        return false;
+
+    }
+};
