bff0a9f4e00edafffdff60c6fd27923897e74f2c
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2026-09-13 00:21:57 +0530

    Time: 161 ms (50.52%) | Memory: 123.9 MB (83.41%) - LeetSync

--- 4317-count-subarrays-with-distant-sums/count-subarrays-with-distant-sums.cpp
+++ 4317-count-subarrays-with-distant-sums/count-subarrays-with-distant-sums.cpp
@@ -1,13 +1,6 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace std;
-using namespace __gnu_pbds;
 class Solution { 
 public: 
     long long distantSubarrays(vector<int>& nums, int goal, int k) { 
-        
          // for each l i need to found r such that abs(sum-goal)>=k 
         //  no of r whose prefix sum - currsum >= x1 
         // and no of r whose prefix sum - currsum <= x2 
@@ -15,26 +8,47 @@ public:
         // x1 and x2 goal se k units dur wale points hai  
 
         int n = nums.size();
-        if(k == 0) return 1LL * n * (n + 1) / 2;
         vector<long long> p(n + 1);
-
+if(k == 0) return 1LL * n * (n + 1) / 2;
         for(int i = 0; i < n; i++) p[i + 1] = p[i] + nums[i];
 
-        using T = pair<long long,int>;
-        tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> s;
+        vector<long long> v = p;
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
 
-        for(int i = 0; i <= n; i++) s.insert({p[i], i});
+        int m = v.size();
+        vector<int> bit(m + 1);
+
+        auto add = [&](int x, int val){
+            for(; x <= m; x += x & -x) bit[x] += val;
+        };
+
+        auto sum = [&](int x){
+            int r = 0;
+            for(; x > 0; x -= x & -x) r += bit[x];
+            return r;
+        };
+
+        for(auto x : p){
+            int id = lower_bound(v.begin(), v.end(), x) - v.begin() + 1;
+            add(id, 1);
+        }
 
         long long ans = 0;
 
         for(int i = 0; i < n; i++){
-            s.erase({p[i], i});
+            int id = lower_bound(v.begin(), v.end(), p[i]) - v.begin() + 1;
+            add(id, -1);
 
             long long x1 = p[i] + goal + k;
             long long x2 = p[i] + goal - k;
 
-            ans += s.size() - s.order_of_key({x1, -1});
-            ans += s.order_of_key({x2, n + 1});
+            int a = lower_bound(v.begin(), v.end(), x1) - v.begin();
+            int b = upper_bound(v.begin(), v.end(), x2) - v.begin();
+
+            ans += sum(m) - sum(a);
+            ans += sum(b);
+
         }
 
         return ans;
